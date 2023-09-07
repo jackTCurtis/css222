4186c9bb02961b91965371542a56915e092fcde6
Jackson Curtis <puajr47@gmai.com>
2023-09-07 08:58:51 -0400

    added ten_mathmeatical_expression.cpp

---
+++ Exercises/ch02/ten_mathematical_expression.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+int n = rand() % 10;
+int c = rand() % 5;
+int b = rand() % 10 +15;
+int a = rand() % 1+3;
+int d = rand() % 7+13;
+
+count << "The product of " << n << " and " << c " is ";
+count << n * c << "." << endl;
+
+return 0;
+}
