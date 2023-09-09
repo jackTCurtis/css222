a91af136c244504900995f706f1dc4a0666bdcc9
994833 <994833@994833-student-fvfd81y5j1wk.fios-router.home>
2023-09-09 15:54:22 -0400

    added base verision for the first exercise

---
+++ Exercises/ch03/exercise1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void new_line()
+{
+	cout << endl;
+}
+
+int main()
+{
+cout << "first Line." << endl;
+newLine();
+cout << "Second Line."<< endl;
+return 0;
+}
