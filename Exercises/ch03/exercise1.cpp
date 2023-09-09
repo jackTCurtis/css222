5c933ff0313f5402eaac9527e2c1d615d241e698
994833 <994833@994833-student-fvfd81y5j1wk.fios-router.home>
2023-09-09 16:01:24 -0400

    changed exercise 1 to fit with the constrait, im guessing it will return an error as it will read main first but I have no Idea

--- Exercises/ch03/exercise1.cpp
+++ Exercises/ch03/exercise1.cpp
@@ -2,15 +2,15 @@
 #include <math.h>
 using namespace std;
 
-void new_line()
-{
-	cout << endl;
-}
-
 int main()
 {
 cout << "first Line." << endl;
 new_line();
 cout << "Second Line."<< endl;
 return 0;
+}
+
+void new_line()
+{
+	cout << endl;
 }
