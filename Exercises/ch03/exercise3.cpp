22feb75231d5f71b3b1227f98d8636112766fa66
994833 <994833@994833-student-fvfd81y5j1wk.fios-router.home>
2023-09-10 11:28:57 -0400

    added my version of exercise 3, going to finish it in a minute just installing vscode first

---
+++ Exercises/ch03/exercise3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#inlcude <math.h>
+using namespace std;
+
+void rndmnumber generator(int inp1 int inp2) {
+if (inp2 == null) {
+inp2 = rand() % 100;
+cout << "Your summary is " << inp1 << "+" << inp2 << "=" << inp1 + inp2 << endl;
+}
+
+int main(){
+
+}
+
