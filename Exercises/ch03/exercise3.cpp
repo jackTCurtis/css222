bc0d72efaf7cb6c7faea944237f47334943fb1bd
Jackson Curtis <puajr47@gmail.com>
2023-09-10 11:50:58 -0400

    updated exercise 3, wont be las tupdate. I've learned c++'s null is just 0, havent run this to see of it works yet

--- Exercises/ch03/exercise3.cpp
+++ Exercises/ch03/exercise3.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#inlcude <math.h>
+#include <math.h>
 using namespace std;
 
-void rndmnumber generator(int inp1 int inp2) {
-if (inp2 == null) {
+void rndmnumbergenerator (int inp1, int inp2) {
+if ((inp2 == 0)&&(!(inp1==0))) {
 inp2 = rand() % 100;
 cout << "Your summary is " << inp1 << "+" << inp2 << "=" << inp1 + inp2 << endl;
 }
-
+}
 int main(){
-
+rndmnumbergenerator(10,0);
+return 0;
 }
 
