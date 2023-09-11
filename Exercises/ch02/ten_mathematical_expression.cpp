8027afdbf2ea12349241ab66ef346b0e8dce0c63
Jackson Curtis <puajr47@gmail.com>
2023-09-10 21:54:03 -0400

    fixed issue with ten mathmatetical expressions. Fixed rand seed staying the same and typo with cout

--- Exercises/ch02/ten_mathematical_expression.cpp
+++ Exercises/ch02/ten_mathematical_expression.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 
 int main() {
+srand(time(NULL));
 int n = rand() % 10;
 int c = rand() % 5;
 int b = rand() % 10 +15;
 int a = rand() % 1+3;
 int d = rand() % 7+13;
 
-count << "The product of " << n << " and " << c " is ";
-count << n * c << "." << endl;
+cout << "The product of " << n << " and " << c << " is ";
+cout << n * c << "." << endl;
 
 return 0;
 }
