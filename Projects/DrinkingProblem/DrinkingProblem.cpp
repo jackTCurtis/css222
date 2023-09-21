5fcb99eca39d1bb9486c5685b8a9ba8fa2f88123
Jackson Curtis <puajr47@gmail.com>
2023-09-21 09:14:32 -0400

    started my first projects, ignore drinking problem we moved on from that project

---
+++ Projects/DrinkingProblem/DrinkingProblem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    /*
+    int numofglasses = 0;
+
+    cout << "Number of Glasses:" << endl;
+    cin >> numofglasses;
+
+    int glassozamounts[numofglasses];
+    int i;
+
+    
+    for (i = 0; i < numofglasses; i++)
+    {
+        cout << "Ozs in glass " << i << ":"<<endl;
+        cin >> numofglasses[i];
+       
+    }
+    
+
+
+    */
+}
