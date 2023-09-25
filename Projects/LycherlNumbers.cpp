aa3c3c5db8ca5eb82efe54068a1e975cee56743d
Jackson Curtis <puajr47@gmail.com>
2023-09-25 01:50:28 -0400

    Project is very close to being finished, the splitter works now which is the hardest part, currently the project doesnt work I think

--- Projects/LycherlNumbers.cpp
+++ Projects/LycherlNumbers.cpp
@@ -11,37 +11,65 @@ int main(){
     cin >> inputnum;
 
     cout << "Define maximum amount of iterations (Hard limit of 25):" << endl;
-    int maxamntofiterations;
-    cin >> maxamntofiterations;
+    int maxops;
+    cin >> maxops;
     //makes sure the input doesnt go over or under limit
-    if (maxamntofiterations > 25){
-        maxamntofiterations = 25;
+    if (maxops > 25){
+        maxops = 25;
         cout << "Max amount is too high, set to 25";
-    }else if (maxamntofiterations < 1){
-        maxamntofiterations = 1;
+    }else if (maxops < 1){
+        maxops = 1;
         cout << "Max amount was too low, set to 1";
     }
     
-    string lefthalf;
-    string righthalf;
+    string lefthalf = "";
+    string righthalf = "";
 
+    //beacuase of some bs c++ syntax i need to make a bufferstring thats just a string version of inputnum
+    string bufferstring = (to_string(inputnum));
+    cout << "----" << endl;
 
-    if((to_string(inputnum)).length() % 2 == 0){
-        //input is even
-        for (int i = 0;i < ((to_string(inputnum)).length()); i++ ){
-            if (i < (((to_string(inputnum)).length())/2)){
-                //i is still in the left half
-                lefthalf[i]=(to_string(inputnum))[i];
-            } if (i > (((to_string(inputnum)).length())/2)){
-                //i is still in the right half
-                righthalf[i]=(to_string(inputnum))[i];
+    int e = 0;
+    bool Lycherbool = false;
+    while ((e!=(maxops))&&(Lycherbool == false)){
+        //multiplyer
+        inputnum = inputnum * 2;
+        bufferstring = (to_string(inputnum));
+
+        //seperater
+        for (int i = 0;i < to_string(inputnum).length(); i++ ){
+            if((to_string(inputnum)).length() % 2 == 0){
+                //input is even
+                if (i < ((to_string(inputnum)).length()/2)){
+                    //i is still in the left half
+                    lefthalf = lefthalf + bufferstring[i];
+                } if (i >= ((to_string(inputnum)).length()/2)){
+                    //i is still in the right half
+                    righthalf = righthalf + bufferstring[i];
+                }
+                
+            } else{
+            //input is odd
+            if (i < ((to_string(inputnum)).length()/2)){
+                    //i is still in the left half
+                    lefthalf = lefthalf + bufferstring[i];
+                } if (i > ((to_string(inputnum)).length()/2)){
+                    //i is still in the right half
+                    //this will skip the middle value
+                    righthalf = righthalf + bufferstring[i];
+                }
+            
             }
-        }
-        cout << "blah blah" <<endl;
-    } else{
-        //input is odd
     }
-    cout << lefthalf << "+" << righthalf;
+    
+        //chechker
+        if (lefthalf == righthalf){
+            Lycherbool = true;
+            cout << "After " << e << " iterations, your input has reached a non-Lycherl number of:" << inputnum;
+        }else{
+        e++;
+        }
+    }
 
 
 }
