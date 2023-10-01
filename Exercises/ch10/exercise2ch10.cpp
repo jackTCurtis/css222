4b28079ddb95ec87bae0959c763c8839206aec6f
Jackson Curtis <puajr47@gmail.com>
2023-09-30 22:05:59 -0400

    made second exercise results are very weighted towards the lower numbers

---
+++ Exercises/ch10/exercise2ch10.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+int how_many(const vector<int>& vec, int value)
+{
+    int count = 0;
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] == value) count++;
+    }
+    return count;
+
+}
+vector<int> random_vector(int n, int upper_bound)
+{
+    vector<int> vec(n);
+
+    for (int i = 0; i < vec.size(); i++) {
+        vec[i] = rand() % upper_bound;
+    }
+
+    return vec;
+}
+int main(){
+    int num_values = 20;
+    int upper_bound = 10;
+    vector<int> vector = random_vector(num_values, upper_bound);
+    cout << "value\thow_many" << endl;
+    for (int i = 0; i < upper_bound; i++) {
+    cout << i << '\t' << how_many(vector, i) << endl;
+}
+}
