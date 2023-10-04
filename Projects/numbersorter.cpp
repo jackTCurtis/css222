5c8238cf2bc68b735b045491a36a8550115fea5c
Jackson Curtis <puajr47@gmail.com>
2023-10-04 09:08:04 -0400

    added swaps.cpp the first exerise for ch11 aswell as the new project

---
+++ Projects/numbersorter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <array>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int inputamnt;
+
+    cout << "How many input int's do you want:";
+    cin >> inputamnt;
+
+    int inputarray[inputamnt];
+
+    for (int i = 0; i < inputamnt; i++)
+    {
+        cout << "Your input for a string";
+        cin >> inputarray[i];
+    }
+    
+}
