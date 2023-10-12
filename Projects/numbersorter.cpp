e9cd5091df2d5b7ac9f929a1140c25099d840293
Jackson Curtis <puajr47@gmail.com>
2023-10-12 00:40:33 -0400

    finished pointer project

--- Projects/numbersorter.cpp
+++ Projects/numbersorter.cpp
@@ -6,15 +6,22 @@ using namespace std;
 int main(){
     int inputamnt;
 
-    cout << "How many input int's do you want:";
+    cout << "How many input int's do you want: ";
     cin >> inputamnt;
 
     int inputarray[inputamnt];
 
     for (int i = 0; i < inputamnt; i++)
     {
-        cout << "Your input for a string";
+        cout << "Your input for array index " << i << ": ";
         cin >> inputarray[i];
     }
+
+    //filter1   
+    for (int i = 0; i < sizeof(inputarray) / sizeof(int); i++)
+    {
+        cout << inputarray[i] << "\t"<< "Location: "<<long(&inputarray[i])<<"\n";
+    }
+    
     
 }
