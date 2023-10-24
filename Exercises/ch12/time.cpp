46d47f514d1a0b502bb04293b353fbc7240965ed
Jackson Curtis <puajr47@gmail.com>
2023-10-24 03:44:58 -0400

    exercise 1 is upto-date with the lesson  before the actual exercise

--- Exercises/ch12/time.cpp
+++ Exercises/ch12/time.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 #include <time.h>
+#include <cmath>
 using namespace std;
 
 struct Time {
     int hour, minute;
     double second;
+
+    void print();
+    void increment(double secs);
 };
-void print_time(const Time& time){
-    cout << time.hour << ":" << time.minute << ":" << time .second << endl;
+double Time::convert_to_seconds() const
+{
+    return ((hour * 60) + minute) * 60 + second;
+}
+void Time::print()
+{
+    cout << hour << ":" << minute << ":" << second << endl;
+}
+void Time::increment(double secs){
+    second += secs;
+
+    while (second >= 60.0)
+    {
+        second -= 60.0;
+        minute += 1;
+    }
+    while (minute >= 60)
+    {
+        minute -= 60;
+        hour += 1;
+    }
+    
+    
 }
 int main(){
     Time current_time = {9 ,14 ,30.0};
-    print_time(current_time);
+    current_time.increment(500.0);
+    current_time.print();
 }
