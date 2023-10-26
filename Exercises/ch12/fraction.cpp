dbf8c6f82ce1a3053326489dd5cdfe3310d25ae9
Jackson Curtis <puajr47@gmail.com>
2023-10-26 08:46:45 -0400

    Fraction is finally done, thank you tdog for helping me out I think I finally understand oop

--- Exercises/ch12/fraction.cpp
+++ Exercises/ch12/fraction.cpp
@@ -2,7 +2,34 @@
 #include <fraction.h>
 using namespace std;
 
-int main(){
-    Fraction myclone(10,2);
-    Fraction print;
+Fraction::Fraction(int n, int d){
+    numerator = n;
+    denominator = d;
+}
+
+void Fraction::print(){
+    cout << "Numerator:" << numerator << endl <<"Denominator:" << denominator << endl; 
+
+}
+void Fraction::plus(Fraction& fruct){
+    numerator *= fruct.denominator;
+    fruct.numerator *= denominator;
+    denominator *= fruct.denominator;
+    numerator += fruct.numerator;
+}
+void Fraction::minus(Fraction& fruct) {
+    numerator *= fruct.denominator;
+    fruct.numerator *= denominator;
+    denominator *= fruct.denominator;
+    numerator -= fruct.numerator;
+}
+
+void Fraction::times(Fraction& fruct) {
+    numerator *= fruct.numerator;
+    denominator *= fruct.denominator;
+}
+
+void Fraction::divided_by(Fraction& fruct) {
+    numerator *= fruct.denominator;
+    denominator *= fruct.numerator;
 }
--- Exercises/ch12/fraction.h
+++ Exercises/ch12/fraction.h
@@ -1,14 +1,9 @@
 struct Fraction {
-    int numerator;
-    int denominator;
-    void print(){
-        cout << "Numerator:" << numerator << endl <<"Denominator:" << denominator << endl; 
-    };
-    void plus(Fraction fruct){
-        
-    };
-
-    void minus(Fraction fruct);
-    void times(Fraction fruct);
-    void divide_by(Fraction fruct);
+    int numerator, denominator;
+    Fraction(int n, int d);
+    void print(Fraction& fruct);
+    void plus(Fraction& fruct);
+    void minus(Fraction& fruct);
+    void times(Fraction& fruct);
+    void divide_by(Fraction& fruct);
 };
