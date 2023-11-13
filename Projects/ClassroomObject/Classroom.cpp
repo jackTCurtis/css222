1bcbddfaec51964d4f24c1c7976dc8705f559cbc
Jackson Curtis <puajr47@gmail.com>
2023-11-13 09:18:41 -0500

    Worked on classroom object, for group project, currently theirs 3 things that are gonna be made when making an object, teacher name, classroomtype and a list of the students names

---
+++ Projects/ClassroomObject/Classroom.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <time>
+#include <Classroom.h>
+using namespace std;
+
+
+void Classroom::GenerateClass(Classroom& obligitoryarguement){
+Classroom::TeacherName = Classroom::StudentList[rand()%Classroom::StudentList.size()]
+//allows for any new additons to the list of names still be in the pool of names that can be picked
+Classroom::ClassroomType=Classroom::TypeofClass[rand()%Classroom::TypeofClass.size()]
+Classroom::NumofStudents=rand()%35;
+//need to generate studentes
+}
