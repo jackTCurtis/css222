4552a37fb3db03726a8b5a4e0fba7a12f6dd1508
Jackson Curtis <puajr47@gmail.com>
2023-12-20 08:45:21 -0500

    fucntion works now, basically the whole assignment is done I just need to do a bit more

--- Projects/Duration/Duration.cpp
+++ Projects/Duration/Duration.cpp
@@ -14,29 +14,31 @@ void Duration::to_string(){
     {
     if ((digit - 86400)>=0)
     {
-        digit =digit- 86400;
+        digit = digit- 86400;
         days=days+1;
         //theyre 86400 seconds in a day
-        cout << days;
     }else if((digit - 3600)>=0){
         digit =digit- 3600;
         hours=hours+1;
         //theyre 3600
-        cout << hours;
+        
     }else if((digit - 60)>=0){
         digit =digit- 60;
-        days = days + 1;
+        minutes = minutes + 1;
         //theyre 60 sceonds in a day
-        cout << days;
+       
     }else{ 
         seconds=digit;
         digit = 0;
-        cout << seconds;
-    }}}
+       
+    }}
+    
+    cout<< "output:"<<endl<<"days: "<<days<<endl<<"hours: "<<hours<<endl<<"minutes: "<<minutes<<endl<<"seconds: "<<seconds;
+    }
     
 }
 int main(){
 Duration tinyobj;
-tinyobj.digit = 500;
+tinyobj.digit = 3600;
 tinyobj.to_string();
 }
