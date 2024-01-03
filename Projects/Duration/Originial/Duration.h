f8f88b85646d7bef042543cbbc0acb632b9b6efc
Jackson Curtis <puajr47@gmail.com>
2024-01-03 08:52:28 -0500

    added a white text culleer for string, as well made a backup, will need to make a major reqrite to both files

---
+++ Projects/Duration/Originial/Duration.h
@@ -0,0 +1,11 @@
+using namespace std;
+class Duration{
+    public:
+    int digit;
+    int days = 0;
+    int hours = 0;
+    int minutes = 0;
+    int seconds = 0;
+    void to_string();
+
+};
---
+++ Projects/Duration/Originial/testprogram.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 800;
+
+    if((x-400)>0){
+        x - 400;
+        cout<<x;
+    }
+}
