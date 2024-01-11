5017da50d218810503ab45146d2e907354f1df45
Jackson Curtis <puajr47@gmail.com>
2024-01-11 00:02:56 -0500

    customized border so the vertical borders say g

--- Projects/EndSemProject/Ncurses/Lesson4/NCwindow2.cpp
+++ Projects/EndSemProject/Ncurses/Lesson4/NCwindow2.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main(int argc,char ** argv){
     ///initilizes the screen
     //setsup memory and clears the screen
-    initscr();
+    	initscr();
+	cbreak();
     //newwin(height, width, start_y, start_x);
     WINDOW * win = newwin(10,20,10,10);
 	refresh();
-	box(win,0,0);
+	box(win,103,0);
 	mvwprintw(win,1,1,"hello window");
 	wrefresh(win);
     	//refreshes the screen to match it to whats on memory
