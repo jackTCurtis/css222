909578972d8b0d9ae035a6879fc749f9905f1ad1
Jackson Curtis <puajr47@gmail.com>
2024-01-11 00:27:28 -0500

    it is statically typed, but if you use '' instead of  to signify a char youre good, I guess strings just dont work which sucks

--- Projects/EndSemProject/Ncurses/Lesson4/NCwindow2.cpp
+++ Projects/EndSemProject/Ncurses/Lesson4/NCwindow2.cpp
@@ -10,7 +10,7 @@ int main(int argc,char ** argv){
     WINDOW * win = newwin(10,20,10,10);
 	refresh();
 	//box(win,0,0);
-	wborder(win,104,104,103,103,97,97,97,97);
+	wborder(win,'c',104,103,103,97,97,97,97);
 	mvwprintw(win, 1,1, "this is a box yes");
 	wrefresh(win);
     	//refreshes the screen to match it to whats on memory
