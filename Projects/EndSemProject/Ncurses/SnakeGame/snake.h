96e9f975a2683fdddd5afbcaf7a576afdf2b98ad
Jackson Curtis <puajr47@gmail.com>
2024-01-28 12:18:44 -0500

    returned files to previous version starting back from here

--- Projects/EndSemProject/Ncurses/SnakeGame/snake.h
+++ Projects/EndSemProject/Ncurses/SnakeGame/snake.h
@@ -1,39 +1,24 @@
 using namespace std;
-
 class Board{
     
     public:
-    int secondsClock;
-    chtype Direction; //h = left j = up k = down l right
-    int SnakeX, SnakeY;
-    WINDOW *boardWindo;
-
     Board(int height, int width){
         int tHeight;
         int tLength;
         getmaxyx(stdscr,tHeight,tLength);
-        
         boardWindo = newwin(height,width,(tHeight/2)-(height/2),(tLength/2)-(width/2));
         initlize();
-
-
         this->height = height;
         this->width = width;
-
     }
     void initlize(){
-        SnakeX = 15;
-        SnakeY = 15;
         clear();
         refresh();
-    }
-    void checkDirection(){
-        Direction = wgetch(boardWindo);
-        // thiw will get other keys being pressed but only hjkl are important
     }
     void addBorder(){
         box(boardWindo,0,0);
     }
+
     void addAt(int y, int x, chtype ch){
         mvwaddch(boardWindo,y,x,ch);
     }
@@ -47,8 +32,7 @@ class Board{
     void refresh(){
         wrefresh(boardWindo);
     }
-    
     private:
-   
+    WINDOW *boardWindo;
     int height, width;
 };
--- Projects/EndSemProject/Ncurses/SnakeGame/vSnake.cpp
+++ Projects/EndSemProject/Ncurses/SnakeGame/vSnake.cpp
@@ -1,76 +1,20 @@
 #include <ncurses.h>
 #include "snake.h"
-#include <iostream>
 using namespace std;
-
 #define BOARD_DIM 20
 #define BOARD_ROW BOARD_DIM
 #define BOARD_COLS BOARD_DIM * 2.5
-
 //snake through vim keybinds, h,j,k,l
 int main(int argc, char ** argv){
 initscr();
 refresh();
-bool colorFlipper = true;
+
+Board baord(BOARD_ROW,BOARD_COLS);
 Board board(BOARD_ROW,BOARD_COLS);
 board.initlize();
-/*
-25 supdates will kill the game so once it hits 25
-it will set back to 1, the idea is to be able to kill the 
-game if a player looses it will set it too 26 seconds.
-*/
-/*
-this is the main updater for this current version
-*/
-cbreak();
-noecho();
+board.addAt(5,5,'0');
+board.refresh();
 
-board.secondsClock = 0;
-while (board.secondsClock<26)
-{
-    board.secondsClock=board.secondsClock+1;
-    if (board.secondsClock ==  25)
-    {
-        if (colorFlipper == true)
-        {
-            attron(A_REVERSE);
-            board.addAt(board.SnakeX,board.SnakeY,'0');
-            colorFlipper = false;
-        }
-        else{
-            attroff(A_REVERSE);
-            board.addAt(board.SnakeX,board.SnakeY,'0');
-            colorFlipper = true;
-        }
-        wrefresh(*boardWindo);
-    /*
-    switch (board.Direction){
-        case 'h':
-            board.SnakeX--;
-            board.addAt(board.SnakeY,board.SnakeX,'0');
-            break;
-        case 'j':
-            board.SnakeY++;
-            board.addAt(board.SnakeY,board.SnakeX, '0');
-            break;
-        case 'k':
-            board.SnakeY--;
-            board.addAt(board.SnakeY,board.SnakeX, '0');
-            break;
-        case 'l':
-            board.SnakeX++;
-            board.addAt(board.SnakeY,board.SnakeX, '0');
-            break;
-    }*/
-    board.secondsClock = 0;
-    }
-    board.refresh();
-
-}
-
-
-
-getch();
 getch();
 endwin();
 return 0;
