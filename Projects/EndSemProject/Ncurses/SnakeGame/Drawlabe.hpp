f159b9fc36233fa6209d8246d6070f630d14263c
Jackson Curtis <puajr47@gmail.com>
2024-01-28 18:55:34 -0500

    added drawlabe header file

---
+++ Projects/EndSemProject/Ncurses/SnakeGame/Drawlabe.hpp
@@ -0,0 +1,28 @@
+#pragma once
+//this header file is to draw everything that needs to be drawed
+//it could've been fit into the vSnake Header but it would've been a mess
+//of member functions
+class Drawlable {
+    public:
+    Drawlable(){
+        y = x = 0; // top left corner by defualt
+        icon = ' '; // blank
+    }
+    Drawlable(int y, iny x, chtype ch){
+        this->y=y;
+        this->x=x;
+        this->icon = ch;
+    }
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+    chtype getIcon(){
+        return icon;
+    }
+    protected:
+    int y, x;
+    chtype icon;
+};
